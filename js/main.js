e844065499124d813f8437d9e07265a8b8aac802
masataka <match.suzuki@gmail.com>
2016-07-06 22:35:42 +0900

    アイコン変更

--- js/main.js
+++ js/main.js
@@ -49,21 +49,98 @@ $(function(){
 	});
 
 	//メニューバースクロール制御
-//	$(window).on('scroll',function(){
-//  	var scrollTop = $(window).scrollTop();
-//  	if(scrollTop > 300){
-//   		$('.navbar').stop().animate({
-//  			backgroundColor:'white'
-//  		},500
-//  		);
-//  		$('.mainMenu').switchClass('mainMenu', 'mainMenuScroll', 1000);
-//  		return false;
-//  	}
-//  	if(scrollTop == 0){
-//   		$('.nav li').css({
-//  			backgroundColor:'transparent'
-//  		});
-// 			$('.mainMenuScroll').switchClass('mainMenuScroll', 'mainMenu', 1000);
-//  	}
-//	});
+	$(window).on('scroll',function(){
+ 	var scrollTop = $(window).scrollTop();
+ 	if(scrollTop > 320){
+  		$('.navbar').stop().animate({
+ 			backgroundColor:'white'
+ 		},500
+ 		);
+ 		$('.nav li a').stop().animate({
+ 			color:'#003366'
+ 		},500
+ 		);
+ 		$('.mainMenu').switchClass('mainMenu', 'mainMenuScroll', 1000);
+ 		
+ 		return false;
+ 	}
+ 	if(scrollTop == 0){
+  		$('.navbar').stop().animate({
+ 			backgroundColor:'transparent'
+ 		});
+ 		$('.nav li a').stop().animate({
+ 			color:'white'
+ 		},500
+ 		);
+ 		$('.mainMenuScroll').switchClass('mainMenuScroll', 'mainMenu', 1000);
+ 	}
+	});
+
+	//partnerMAILアイコン
+	$(".partner span")
+		.on('mouseover', function(){
+			$('#partnerBlock').stop().animate({
+				opacity:'1.0'
+			},500
+			);
+			$('#partnerComent').stop().animate({
+				opacity:'1.0'
+			},500
+			);
+		}).on('mouseout', function(){
+			$('#partnerBlock').stop().animate({
+				opacity:'0.8'
+			},500
+			);
+			$('#partnerComent').stop().animate({
+				opacity:'0'
+			},500
+			);
+		});
+
+	//contactMAILアイコン
+	$(".mail span")
+		.on('mouseover', function(){
+			$('#mailBlock').stop().animate({
+				opacity:'1.0'
+			},500
+			);
+			$('#mailComent').stop().animate({
+				opacity:'1.0'
+			},500
+			);
+		}).on('mouseout', function(){
+			$('#mailBlock').stop().animate({
+				opacity:'0.8'
+			},500
+			);
+			$('#mailComent').stop().animate({
+				opacity:'0'
+			},500
+			);
+		});
+
+	//contactTELアイコン
+	$("#telBlock")
+		.on('mouseover', function(){
+			$('#telBlock').stop().animate({
+				opacity:'1.0'
+			},500
+			);
+			$('#telComent').stop().animate({
+				opacity:'1.0'
+			},500
+			);
+		}).on('mouseout', function(){
+			$('#telBlock').animate({
+				opacity:'0.8'
+			},500
+			);
+			$('#telComent').stop().animate({
+				opacity:'0'
+			},500
+			);
+		});
+
+
 });
