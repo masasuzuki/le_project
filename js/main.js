7a9fe900d969e57f9fd9c3e8d14c52330faf2496
masasuzuki <match.suzuki@gmail.com>
2018-08-17 17:13:53 +0900

    顧問弁護士追記

--- js/main.js
+++ js/main.js
@@ -200,5 +200,14 @@ $(function(){
 			);
 		});
 
-
+	//IEスムーススクロール禁止（background-attachment : fixed対策）
+	if(navigator.userAgent.match(/MSIE 10/i) || navigator.userAgent.match(/Trident\/7\./) || navigator.userAgent.match(/Edge\/12\./)) {
+		$('body').on("mousewheel", function () {
+			event.preventDefault();
+			var wd = event.wheelDelta;
+			var csp = window.pageYOffset;
+			window.scrollTo(0, csp - wd);
+		});
+	}
 });
+
